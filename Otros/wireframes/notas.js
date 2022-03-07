bcdceb6fabe82e5d987ca8895bb54e298a166d80
joaquin1990 <joaquingarese@gmail.com>
2022-03-07 18:17:03 -0300

    Modifique los scss, pase al general lo que se repetia y era completamente igual

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -1,6 +1,4 @@
 // -
-// -
-// -
 // - Hacer un logo real con algun programa.
 // - Hacer que las animaciones del nav no funcionen para cuando se hace chico el
 //   menu chico, capaz hacer una media querie intermedia para eso.
@@ -8,17 +6,12 @@
 // -
 // - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
 // - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
-// - Acordarme de simplificar el scss para que quede todo en (capaz sacar el scss y dejar solamente css)
-// - Eliminar las imagenes que no se usen, y acomodar las imagenes del carrousel, hacerlas mas economicas.
-// - Acomodar los margenes de nosotros en mobile xs.
 // - Agregar un href que lleve al inicio sobre el titulo y sobre el logo tambien.
 // - Chequear todas las imagenes y agregarles el alt a todas (la del titulo, etc)
-// - Achicar la imagen del logo (hacer el logo?) (chequearla en todas las paginas).
 // - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
-// - Sacar los wireframes de la carpeta, y sacar el codigo de js con las notas tambien para el proyecto final.
 // - Ver que hacer con las imagenes en el carrousel, si partirlo en dos o que hacer.
-// - Separar loguitos de footer, separarlos con clase me-1 en los i como esta hecho en la pagina de inicio.
 // - hover a los links de abajo para que se subrayen cuando estamos encima.
-// -
+// - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
+// - distintos tamaños
 // -
 // -
