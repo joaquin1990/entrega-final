3e1546815a5c582359bfc0da805993b712fe7383
joaquin1990 <joaquingarese@gmail.com>
2022-03-07 16:16:27 -0300

    Ajustes esteticos en pag inicio

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -5,12 +5,10 @@
 // - Hacer que las animaciones del nav no funcionen para cuando se hace chico el
 //  menu chico, capaz hacer una media querie intermedia para eso.
 // - Modificar la pagina de estilos generales, y las cosas que apliquen para los dos
-// - responsive dejarlos en la de estilos generales y chau, o ver que vale mas la pena,
-// - capaz preguntarle a fede.
 // -
+// - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
 // - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
 // - Acordarme de simplificar el scss para que quede todo en (capaz sacar el scss y dejar solamente css)
-// - Anotar los pasos que vi en el after de github para que subir la pagina a una pag que se pueda ver.
 // - Eliminar las imagenes que no se usen, y acomodar las imagenes del carrousel, hacerlas mas economicas.
 // - Acomodar los margenes de nosotros en mobile xs.
 // - Agregar un href que lleve al inicio sobre el titulo y sobre el logo tambien.
@@ -18,7 +16,6 @@
 // - Achicar la imagen del logo (hacer el logo?) (chequearla en todas las paginas).
 // - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
 // - Sacar los wireframes de la carpeta, y sacar el codigo de js con las notas tambien para el proyecto final.
-// - Avisarle a alejandro que empiecen a producir, que la semana que viene vamos a necesitar algunas cajas.
 // - Pasar los piques del pipe a y el resto de las cosas a retocar al js de notas.
 // - Ver que hacer con las imagenes en el carrousel, si partirlo en dos o que hacer.
 // - Margenes en ambos lados de la pag iguales (probar clase container a ver que pasa)
