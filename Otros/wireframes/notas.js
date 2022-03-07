42d156a40aa67880e13b3c92a0c8f07361f1d695
joaquin1990 <joaquingarese@gmail.com>
2022-03-07 15:03:50 -0300

    Primer commit

---
+++ Otros/wireframes/notas.js
@@ -0,0 +1,31 @@
+// -
+// -
+// -
+// - Hacer un logo real con algun programa.
+// - Hacer que las animaciones del nav no funcionen para cuando se hace chico el
+//  menu chico, capaz hacer una media querie intermedia para eso.
+// - Modificar la pagina de estilos generales, y las cosas que apliquen para los dos
+// - responsive dejarlos en la de estilos generales y chau, o ver que vale mas la pena,
+// - capaz preguntarle a fede.
+// -
+// - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
+// - Acordarme de simplificar el scss para que quede todo en (capaz sacar el scss y dejar solamente css)
+// - Anotar los pasos que vi en el after de github para que subir la pagina a una pag que se pueda ver.
+// - Eliminar las imagenes que no se usen, y acomodar las imagenes del carrousel, hacerlas mas economicas.
+// - Acomodar los margenes de nosotros en mobile xs.
+// - Agregar un href que lleve al inicio sobre el titulo y sobre el logo tambien.
+// - Chequear todas las imagenes y agregarles el alt a todas (la del titulo, etc)
+// - Achicar la imagen del logo (hacer el logo?) (chequearla en todas las paginas).
+// - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
+// - Sacar los wireframes de la carpeta, y sacar el codigo de js con las notas tambien para el proyecto final.
+// - Avisarle a alejandro que empiecen a producir, que la semana que viene vamos a necesitar algunas cajas.
+// - Pasar los piques del pipe a y el resto de las cosas a retocar al js de notas.
+// - Ver que hacer con las imagenes en el carrousel, si partirlo en dos o que hacer.
+// - Margenes en ambos lados de la pag iguales (probar clase container a ver que pasa)
+// - Separar loguitos de footer, separarlos con clase me-1 en los i como esta hecho en la pagina de inicio.
+// - hover a los links de abajo para que se subrayen cuando estamos encima.
+// - Mas cariño a los articles de inicio?
+// - Alinear redes sociales y golf academy en el footer que queden igual altura etc.
+// -
+// -
+// -
