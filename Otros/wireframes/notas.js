bf14bf3ee9ff5804129966fabfd50e7a08576649
joaquin1990 <joaquingarese@gmail.com>
2022-03-09 16:57:46 -0300

    Varios cambios, animaciones y otros

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -1,15 +1,13 @@
 // -
-// - Hacer que las animaciones del nav no funcionen para cuando se hace chico el
-//   menu chico, capaz hacer una media querie intermedia para eso.
-// - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
 // - Agregar un href que lleve al inicio sobre el titulo y sobre el logo tambien.
 // - Chequear todas las imagenes y agregarles el alt a todas (la del titulo, etc)
 // - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
 // - distintos tamaños
+// - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
 // -
 // - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
 // - Ver que hacer con las imagenes en el carrousel, si partirlo en dos o que hacer.
 // - hover a los links de abajo para que se subrayen cuando estamos encima.
 // - Alinear en footer la marca y las redes que no estan del todo bien todavia
 // - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
-// -
+// - Hacer que el navbar quede fixed siempre?
