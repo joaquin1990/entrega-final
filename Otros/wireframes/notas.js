c55127dbaf5b9cfd6fed50de056d7e5702dcd585
joaquin1990 <joaquingarese@gmail.com>
2022-03-09 16:17:37 -0300

    Arregle las animaciones que habian quedado solamente en la pagina de inicio

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -1,17 +1,15 @@
 // -
-// - Hacer un logo real con algun programa.
 // - Hacer que las animaciones del nav no funcionen para cuando se hace chico el
 //   menu chico, capaz hacer una media querie intermedia para eso.
-// - Modificar la pagina de estilos generales, y las cosas que apliquen para los dos
-// -
-// - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
 // - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
 // - Agregar un href que lleve al inicio sobre el titulo y sobre el logo tambien.
 // - Chequear todas las imagenes y agregarles el alt a todas (la del titulo, etc)
+// - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
+// - distintos tamaños
+// -
 // - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
 // - Ver que hacer con las imagenes en el carrousel, si partirlo en dos o que hacer.
 // - hover a los links de abajo para que se subrayen cuando estamos encima.
-// - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
-// - distintos tamaños
 // - Alinear en footer la marca y las redes que no estan del todo bien todavia
+// - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
 // -
