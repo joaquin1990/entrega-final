50183bf8540121839cff506e5d3e227098425273
joaquin1990 <joaquingarese@gmail.com>
2022-03-09 17:53:59 -0300

    Optimizando el SEO mediante la compresion de las imagenes

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -1,13 +1,12 @@
 // -
-// - Agregar un href que lleve al inicio sobre el titulo y sobre el logo tambien.
-// - Chequear todas las imagenes y agregarles el alt a todas (la del titulo, etc)
 // - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
 // - distintos tamaños
+// - Cambiar la imagen de fondo y la del logo a webp formato.
 // - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
 // -
+// - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
 // - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
 // - Ver que hacer con las imagenes en el carrousel, si partirlo en dos o que hacer.
 // - hover a los links de abajo para que se subrayen cuando estamos encima.
 // - Alinear en footer la marca y las redes que no estan del todo bien todavia
-// - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
 // - Hacer que el navbar quede fixed siempre?
