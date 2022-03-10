f6b046204d865b8f2687078631142e83f39f05e7
joaquin1990 <joaquingarese@gmail.com>
2022-03-10 11:56:48 -0300

    modifique notas de js

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -3,6 +3,4 @@
 // -
 // - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
 // - distintos tamaños
-// - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
-// - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
 // - Partir en dos el carrousel, si partirlo en dos o que hacer.
