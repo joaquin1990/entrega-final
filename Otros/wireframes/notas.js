73d8465f830201615d7c0c98dad4c7be023c5725
joaquin1990 <joaquingarese@gmail.com>
2022-03-10 09:14:23 -0300

    agregue algunas cosas

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -1,12 +1,12 @@
 // -
+// - Alinear en footer la marca y las redes que no estan del todo bien todavia
+// - Hover a los links de abajo para que se subrayen cuando estamos encima.
+// - Hacer que el navbar quede fixed siempre?
+// -
+// -
+// -
 // - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
 // - distintos tamaños
-// - Cambiar la imagen de fondo y la del logo a webp formato.
-// - Ver lo de optimizacion de la pagina, para ver como esta andando el proyecto
-// -
 // - Ver after de github de nuevo para anotar los pasos para pasarla pag a netlify.
 // - Ver de nuevo las clases de dominio y hosting, resumirlas bien en el resumen.
-// - Ver que hacer con las imagenes en el carrousel, si partirlo en dos o que hacer.
-// - hover a los links de abajo para que se subrayen cuando estamos encima.
-// - Alinear en footer la marca y las redes que no estan del todo bien todavia
-// - Hacer que el navbar quede fixed siempre?
+// - Partir en dos el carrousel, si partirlo en dos o que hacer.
