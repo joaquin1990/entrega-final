b9b69721e5ee0b151380f697f7d50bb31104dcc4
joaquin1990 <joaquingarese@gmail.com>
2022-03-10 09:51:21 -0300

    Navbar fixed

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -1,7 +1,6 @@
 // -
-// - Hover a los links de abajo para que se subrayen cuando estamos encima.
 // - Hacer que el navbar quede fixed siempre?
-// -
+// - Agregarle un href a la marca en el footer, asi no queda al pedo el hover
 // -
 // -
 // - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
