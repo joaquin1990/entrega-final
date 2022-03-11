cb90a5695b29bac57f78ab3d8249be05d888cf36
joaquin1990 <joaquingarese@gmail.com>
2022-03-11 20:27:44 -0300

    notas de js

--- Otros/wireframes/notas.js
+++ Otros/wireframes/notas.js
@@ -4,3 +4,7 @@
 // - Modificar la imagen en contacto como lo habia explicado el profesor, poniendo dos imagenes de
 // - distintos tamaños
 // - Partir en dos el carrousel, si partirlo en dos o que hacer.
+// - Dejar margen libre en los bordes
+// - Sacar los margin padding etc para alinear los margenes y desp lo mismo en el footer.
+// - En pagina contacto sacar el width 100%, ver con el object position para centrar la imagen,
+// - en la clase imagenContacto.
