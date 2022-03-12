bac8ec586276b82f3540e0a1b598ec43b77031ca
joaquin1990 <joaquingarese@gmail.com>
2022-03-12 10:05:09 -0300

    Ultimos detalles

--- Otros/wireframes/notas.js
+++
@@ -1,9 +0,0 @@
-// -
-// -
-// -
-// - Dejar margen libre en los bordes
-// - Sacar los margin padding etc para alinear los margenes y desp lo mismo en el footer.
-// - En pagina contacto sacar el width 100%, ver con el object position para centrar la imagen,
-// - en la clase imagenContacto.
-// - Poner un href en la info de la card de contacto
-// -
